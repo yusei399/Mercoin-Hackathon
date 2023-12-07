117a63c2bd10a08bc617aa018b0ef00d2358fe9b
yusei399 <yusei2021@keio.jp>
2023-12-07 20:29:21 +0900

    webカメラ接続

--- app/components/VideoIndex.tsx
+++ app/components/VideoIndex.tsx
@@ -1,31 +1,47 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Avatar from "@mui/joy/Avatar";
 import Button from "@mui/material/Button";
-
 import "../styles/VideoIndex.css";
 
 const VideoIndex = () => {
-  const videoTitle = "動画タイトル";
-  const videoDescription = "とってもおもしろい動画 \n ぜったいに見てね";
+  const [videoTitle] = useState("動画タイトル");
+  const [videoDescription] = useState("とってもおもしろい動画 \n ぜったいに見てね");
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() =>
+   {
+    // カメラストリームを取得
+    const getVideo = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("カメラアクセスエラー:", err);
+      }
+    };
+
+    getVideo();
+
+    // コンポーネントアンマウント時にストリームをリリース
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+      }
+    };
+  }, []);
 
   return (
     <div className="video-container">
-      <iframe
-        width="800"
-        height="450"
-        src="https://www.youtube.com/embed/ibZYkjYiADQ?si=0_qXzp5ax3PFyswx"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+      <video ref={videoRef} width="800" height="450" autoPlay playsInline></video>
 
       <div className="video-info">
         <h2>{videoTitle}</h2>
         <div className="left-info">
           <Avatar src="" size="lg" />
-          <p className="liver-name"> 葛葉 </p>
+          <p className="liver-name">葛葉</p>
         </div>
         <div className="right-info">
           <Button className="register" variant="contained" color="primary">
@@ -39,3 +55,4 @@ const VideoIndex = () => {
 };
 
 export default VideoIndex;
+
