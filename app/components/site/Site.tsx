ecfc065769ee0bb638b9cc66df3b3be5cda3c27e
yusei399 <yusei2021@keio.jp>
2023-12-07 18:28:02 +0900

    store画面

--- app/components/site/Site.tsx
+++ app/components/site/Site.tsx
@@ -1,79 +1,94 @@
 "use client";
-import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
+import {Box, Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Typography, Button } from '@mui/material';
 import React from 'react';
 import { red } from '@mui/material/colors';
 import Image from 'next/image';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
-const data =[
-	{
-		image : "/葛葉.png",
-		title: "EXゲーマーズ5周年記念",
-		content: "【EXゲーマーズ5周年記念】マウスパッド",
-		price: "0.001token",
-	},
-	{
-		image : "/叶.jpg",
-		title: "EXゲーマーズ5周年記念",
-		content: "【EXゲーマーズ5周年記念】ステッカーセット",
-		price : "0.001token",
-	},
-	{
-		image : "/魔界のりりむ.jpg",
-		tite : "EXゲーマーズ5周年記念",
-		content: "【EXゲーマーズ5周年記念】ヘアクリップ",
-		price: "0.001token",
-	},
-	{
-		image: "/キズナアイ.jpg",
-		title: "EXゲーマーズ5周年記念",
-		content: "【EXゲーマーズ5周年記念】アクリルスタンド",
-		price: "0.001token",
-	},
-	{
-		image : "/宝鐘マリン.jpg",
-		title: "EXゲーマーズ5周年記念",
-		content: "【EXゲーマーズ5周年記念】ランダムチェキ風カード",
-		price : "0.001token",
-	},
-	{
-		image : "/宝鐘マリン.jpg",
-		title: "にじフェス2023",
-		content: "【Kuzuha Solo Event ”Kaleidoscope”】B6アクリルパネル【にじフェス2023】",
-		price : "0.001token",
-	}
-]
-
 const Site = () => {
+	const data =[
+		{
+			image : "/store_batch.png",
+			title: "EXゲーマーズ5周年記念",
+			content: "【EXゲーマーズ5周年記念】マウスパッド",
+			price: "0.001token",
+		},
+		{
+			image : "/ステッカー.png",
+			title: "EXゲーマーズ5周年記念",
+			content: "【EXゲーマーズ5周年記念】ステッカーセット",
+			price : "0.001token",
+		},
+		{
+			image : "/ヘアクリップ.png",
+			tite : "EXゲーマーズ5周年記念",
+			content: "【EXゲーマーズ5周年記念】ヘアクリップ",
+			price: "0.001token",
+		},
+		{
+			image: "/アクリルスタンド.png",
+			title: "EXゲーマーズ5周年記念",
+			content: "【EXゲーマーズ5周年記念】アクリルスタンド",
+			price: "0.001token",
+		},
+		{
+			image : "/ランダムチェキ風.png",
+			title: "EXゲーマーズ5周年記念",
+			content: "【EXゲーマーズ5周年記念】ランダムチェキ風カード",
+			price : "0.001token",
+		},
+		{
+			image : "/アクリルパネル.png",
+			title: "にじフェス2023",
+			content: "【Kuzuha Solo Event ”Kaleidoscope”】B6アクリルパネル【にじフェス2023】",
+			price : "0.001token",
+		},
+		{
+			image : "/バッチセット.png",
+			title : "にじフェス2023",
+			content : "【【Kuzuha Solo Event ”Kaleidoscope”】缶バッジセット【にじフェス2023】",
+			price : "0.001token",
+		},
+		{
+			image : "/オルゴール.png",
+			title : "ChroNoiR 5th ANNIVERSARY",
+			content : "【ChroNoiR 5th ANNIVERSARY】オルゴール",
+			price : "0.001token",
+		}
+		
+	]
 	return (
-		<>
-		<Card sx={{maxWidth: 345}}>
+		<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, justifyContent: 'center' }}>
+		{data?.map((item, index) => (
+		  <Card key={index} sx={{ maxWidth: 345, flexBasis: 'calc(33.333% - 16px)' }}>
 			<CardHeader
-				title="test"
-				avatar={
-					<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-						<Image src="/葛葉.png" width={200} height={200} alt="葛葉" style={{ borderRadius: '30px' }} />
-					</Avatar>
-				}
+			  title={item.title}
+			  avatar={
+				<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+				  <Image src="/葛葉.png" width={200} height={200} alt={item.title} style={{ borderRadius: '30px' }} />
+				</Avatar>
+			  }
 			/>
 			<CardMedia
-				component="img"
-				height="194"
-				image="/葛葉.png"
-				alt="Paella dish"
+			  component="img"
+			  height="194"
+			  image={item.image}
+			  alt={item.content}
 			/>
 			<CardContent>
-				<Typography>
-					【EXゲーマーズ5周年記念】マウスパッド
-				</Typography>
+			  <Typography>
+				{item.content}
+			  </Typography>
 			</CardContent>
 			<CardActions disableSpacing>
-				<FavoriteIcon />
+			  <FavoriteIcon />
+			  <Button size="small" >交換</Button>
 			</CardActions>
-		</Card>
-		</>
+		  </Card>
+		))}
+	  </Box>
 	);
-}
+  }
 
 
 export default Site;
