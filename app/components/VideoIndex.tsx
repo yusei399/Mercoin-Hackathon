590fa225ca822400583feebf15a8322e716617e0
jimmyeaaaaah <yurikahirobe@gmail.com>
2023-12-07 16:12:03 +0900

    add superchat function

--- app/components/VideoIndex.tsx
+++ app/components/VideoIndex.tsx
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import Avatar from "@mui/joy/Avatar";
+import Button from "@mui/material/Button";
+
 import "../styles/VideoIndex.css";
 
 const VideoIndex = () => {
@@ -17,8 +20,18 @@ const VideoIndex = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+
       <div className="video-info">
         <h2>{videoTitle}</h2>
+        <div className="left-info">
+          <Avatar src="" size="lg" />
+          <p className="liver-name"> 葛葉 </p>
+        </div>
+        <div className="right-info">
+          <Button className="register" variant="contained" color="primary">
+            チャンネル登録
+          </Button>
+        </div>
         <p>{videoDescription}</p>
       </div>
     </div>
