d44d0bba9dfbab2f170ed569ac0c242e23b4dc56
yusei399 <yusei2021@keio.jp>
2023-12-07 20:35:03 +0900

    元に戻す

--- app/components/VideoIndex.tsx
+++ app/components/VideoIndex.tsx
@@ -1,47 +1,31 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/joy/Avatar";
 import Button from "@mui/material/Button";
+
 import "../styles/VideoIndex.css";
 
 const VideoIndex = () => {
-  const [videoTitle] = useState("動画タイトル");
-  const [videoDescription] = useState("とってもおもしろい動画 \n ぜったいに見てね");
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() =>
-   {
-    // カメラストリームを取得
-    const getVideo = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (err) {
-        console.error("カメラアクセスエラー:", err);
-      }
-    };
-
-    getVideo();
-
-    // コンポーネントアンマウント時にストリームをリリース
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        videoRef.current.srcObject.getTracks().forEach(track => track.stop());
-      }
-    };
-  }, []);
+  const videoTitle = "動画タイトル";
+  const videoDescription = "とってもおもしろい動画 \n ぜったいに見てね";
 
   return (
     <div className="video-container">
-      <video ref={videoRef} width="800" height="450" autoPlay playsInline></video>
+      <iframe
+        width="800"
+        height="450"
+        src="https://www.youtube.com/embed/ibZYkjYiADQ?si=0_qXzp5ax3PFyswx"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
 
       <div className="video-info">
         <h2>{videoTitle}</h2>
         <div className="left-info">
           <Avatar src="" size="lg" />
-          <p className="liver-name">葛葉</p>
+          <p className="liver-name"> 葛葉 </p>
         </div>
         <div className="right-info">
           <Button className="register" variant="contained" color="primary">
