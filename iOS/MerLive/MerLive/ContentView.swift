b9ffeec6f27a8d767fe06bf4aa5d847beb70ca5f
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2023-12-08 16:38:18 +0900

    add iOS

---
+++ iOS/MerLive/MerLive/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MerLive
+//
+//  Created by 五十嵐諒 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatingView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
