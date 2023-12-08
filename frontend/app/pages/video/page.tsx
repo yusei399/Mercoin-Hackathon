6845eec3a5f06f979d8b2270fa87fa4a2d4bb306
jimmyeaaaaah <yurikahirobe@gmail.com>
2023-12-08 15:15:14 +0900

    modify styles

--- frontend/app/pages/video/page.tsx
+++ frontend/app/pages/video/page.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import VideoIndex from "../../components/video/VideoIndex";
 import CommentForm from "../../components/video/CommentForm";
+import Menu from "../../components/menu/Menu";
 import MercariItems from "@/app/components/mercari_items/MercariItems";
 import { MetaMaskProvider } from "@/app/context/MetaMaskContextProvider";
 import "../../styles/Home.css";
 
 export default function Home() {
   return (
-    <div style={{ display: "flex", width: "90%", margin: "0 auto"}}>
-      <MetaMaskProvider>
-        <div style={{ flex: "0 0 65%", margin: "30px 20px" }}>
-          <VideoIndex />
-          <MercariItems />
-        </div>
-        <div style={{ flex: "0 0 10%", margin: "40px 20px" }}>
-          <CommentForm />
-        </div>
-      </MetaMaskProvider>
+    <div>
+      <div className="header" >
+        <Menu />
+      </div>
+      <div style={{ display: "flex", width: "90%", margin: "0 auto" }}>
+        <MetaMaskProvider>
+          <div style={{ flex: "0 0 65%", margin: "30px 20px" }}>
+            <VideoIndex />
+            <MercariItems />
+          </div>
+          <div style={{ flex: "0 0 10%", margin: "40px 20px" }}>
+            <CommentForm />
+          </div>
+        </MetaMaskProvider>
+      </div>
     </div>
   );
 }
